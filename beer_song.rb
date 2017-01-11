6c49e065f1aed3b6342a4aba7d10476e30a9f994
keita mori <keitamori@gmail.com>
2017-01-11 20:04:27 +0100

    [TEST] 1er test passe - resolution du premier cas

---
+++ beer_song.rb
@@ -0,0 +1,7 @@
+class BeerSong 
+  def verse(something)
+    phr1 = "99 bottles of beer on the wall, 99 bottles of beer.\n"
+    phr2 = "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+    phr1 + phr2
+  end
+end
